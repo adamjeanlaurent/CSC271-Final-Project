3e6ff6cd09335565e1a96f764b67bec36671c965
mjackson-1 <54967160+mjackson-1@users.noreply.github.com>
2019-12-08 14:40:02 -0500

    validation form accounts for alphanumeric characters and spaces

--- js/indexFormValidation.js
+++ js/indexFormValidation.js
@@ -18,7 +18,10 @@
 // This ensures that both boxes on the form are filled out
 function validateForm() {
     var email = document.forms["contact"]["email"].value; 
-    var sortAlgo = document.forms["contact"]["sortingAlgos"].value;    
+    var sortAlgo = document.forms["contact"]["sortingAlgos"].value; 
+  
+    // regular expression to match only alphanumeric characters and spaces
+    var re = /^[\w ]+$/;
     
     if (email === "" && sortAlgo === "") {
     alert("Email address and sorting algorithm cannot be blank.");
@@ -33,5 +36,11 @@ function validateForm() {
     if (email === "" && sortAlgo !== "") {
     alert("Email cannot be blank");
     return false;
+    }
+  
+    // validation fails if the input doesn't match our regular expression
+    if(!re.test(sortAlgo)) {
+      alert("Error: Input for sorting algorithm contains invalid characters!");
+      return false;
     }
 }
