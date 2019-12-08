01c8bb487a737b6e93d7ec8d9a523dbef30ab00f
mjackson-1 <54967160+mjackson-1@users.noreply.github.com>
2019-12-08 11:59:39 -0500

    fixed another typo

--- js/indexFormValidation.js
+++ js/indexFormValidation.js
@@ -17,8 +17,8 @@ function ValidateEmail(inputText)
 
 // This ensures that both boxes on the form are filled out
 function validateForm() {
-    var email = document.getElementById("email"); 
-    var sortAlgo = document.getElementById("sortAlgo");    
+    var email = document.getElementById('email'); 
+    var sortAlgo = document.getElementById('sortAlgo');    
     if (email.value === "") {
     alert("The field marked in red cannot be blank");
     return false; 
