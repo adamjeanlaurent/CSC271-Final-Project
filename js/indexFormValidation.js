e08c3e3d1a5cc2d394e86fb11ffb69d33b1904d5
mjackson-1 <54967160+mjackson-1@users.noreply.github.com>
2019-12-08 14:59:39 -0500

    added email validation
    removed sorting algorithm validation

--- js/indexFormValidation.js
+++ js/indexFormValidation.js
@@ -1,27 +1,9 @@
-// Validiates email address taking text as it input
-/*function ValidateEmail(inputText)
-{
-  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if(inputText.value.match(mailformat))
-  {
-    document.form1.text1.focus();
-    return true;
-  }
-  else
-  {
-  alert("You have entered an invalid email address!");
-  document.form1.text1.focus();
-  return false;
-  }
-}*/
-
 // This ensures that both boxes on the form are filled out
 function validateForm() {
     var email = document.forms["contact"]["email"].value; 
     var sortAlgo = document.forms["contact"]["sortingAlgos"].value; 
   
-    // regular expression to match only alphanumeric characters and spaces
-    var re = /^[\w ]+$/;
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     
     if (email === "" && sortAlgo === "") {
       alert("Email address and sorting algorithm cannot be blank.");
@@ -38,17 +20,13 @@ function validateForm() {
       return false;
     }
   
-    // validation fails if the input doesn't match our regular expression
-    // and if the email address is not blank
-    if(!re.test(sortAlgo) && email !== "") {
-      alert("Error: Input for Sorting algorithm contains invalid characters!");
+   if(!email.match(mailformat) && sortAlgo !== "") {
+      alert("Email address is invalid.");
       return false;
     }
   
-    // validation fails if the input doesn't match our regular expression
-   // and if the email address is blank
-   if(!re.test(sortAlgo) && email === "") {
-      alert("Email address cannot be blank. Sorting algorithm contains invalid characters!");
+   if(!email.match(mailformat) && sortAlgo === "") {
+      alert("Email address is invalid. Sorting algorithm cannot be blank.");
       return false;
     }
 }
